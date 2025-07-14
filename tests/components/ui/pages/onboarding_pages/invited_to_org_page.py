759364d519475edefa174e9a64446aea4eaa1647
Ihor Sosnovskyi <ihor.sosnovskyi@sandbx.co>
2025-07-14 16:06:28 +0300

    stability fix

--- tests/components/ui/pages/onboarding_pages/invited_to_org_page.py
+++ tests/components/ui/pages/onboarding_pages/invited_to_org_page.py
@@ -50,4 +50,6 @@ class InvitedToOrgPage(BasePage):
         await btn.hover()
         await self.page.wait_for_timeout(500)
         await btn.click()
-        await self.page.wait_for_timeout(500)
+        self.log("Wait for network idle")
+        await self.page.wait_for_load_state("networkidle")
+        self.log("Network idle done")
