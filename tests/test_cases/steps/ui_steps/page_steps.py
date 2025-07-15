9573a224457cca2bfd9de6fba4cf883ba30cabd3
Ihor Sosnovskyi <ihor.sosnovskyi@sandbx.co>
2025-07-15 23:42:37 +0300

    history collection fixed

--- tests/test_cases/steps/ui_steps/page_steps.py
+++ tests/test_cases/steps/ui_steps/page_steps.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from tests.components.ui.page_manager import PageManager
 from tests.test_cases.steps.ui_steps.page_steps_impl.Invited_to_org_page_steps import (
     InvitedToOrgPageSteps,
@@ -92,6 +94,7 @@ class PageSteps:
         page_manager: PageManager,
         data_manager: DataManager,
     ) -> None:
+        self._step_method_lock = asyncio.Lock()
         self._pm = page_manager
         self._data_manager = data_manager
 
